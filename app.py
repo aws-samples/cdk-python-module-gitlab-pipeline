36da3d36cfc0a8ba5ab5ba4adb4287633ac8f170
Serge Poueme <pouemes@amazon.com>
2023-02-22 17:45:52 -0500

    adding files to code sample

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+
+from cdk_python_module.cdk_workshop_stack import CdkWorkshopStack
+
+
+app = cdk.App()
+CdkWorkshopStack(app, "cdk-workshop")
+
+app.synth()
+
